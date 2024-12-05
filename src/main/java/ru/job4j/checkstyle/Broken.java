b9636d1648bf6834522e79e16ae4d9af1720415f
Alex <x-750f@list.ru>
2024-12-06 01:50:17 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+
+    String name;
+
+    public static final String NEW_VALUE = "";
+
+    public String surname;
+
+    Broken() { }
+
+    void echo() { }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+}
