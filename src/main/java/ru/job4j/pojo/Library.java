a371aa9581e7a9043649ee08316cfe16a8ebbe51
Alex <x-750f@list.ru>
2024-12-12 01:40:51 +0300

    3. Массивы и модели. [#174892 #231783]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -25,7 +25,7 @@ public class Library {
         System.out.println("Только книги Clean code");
         for (int index = 0; index < books.length; index++) {
             tempBook = books[index];
-            if (tempBook.getName().equals("Clean code")) {
+            if ("Clean code".equals(tempBook.getName())) {
                 System.out.printf("Книга %d : %s - %dстр%n", index, tempBook.getName(), tempBook.getNumberOfPages());
             }
         }
